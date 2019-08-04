381ab9854be37ba259f2f6c5646040497de84b23
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-03 23:46:25 -0700

    Update compile script for pipenv based approach

--- compile.sh
+++ compile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-set -e 
-rm -rf venv
-python3 -m venv venv
-. venv/bin/activate
-pip install -r requirements.txt
+set -e
 git clone https://github.com/timothycrosley/attila.git
 pelican-themes -i attila
 rm -rf attila
