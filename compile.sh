bdb57c4d0a5cf67207a99f6bcfda682583a8fac0
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-10 23:38:59 -0700

    Attempt to build within pipenv

--- compile.sh
+++ compile.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
-#git clone git clone git@github.com:mamcmanus/brutalistpelican.git
-#pelican-themes -i brutalistpelican
-#rm -rf brutalistpelican
 rm -rf utterson
 git clone https://github.com/timothycrosley/utterson
-pelican-themes -U utterson
+pipenv run pelican-themes -U utterson
 rm -rf utterson
-pelican
+pipenv run pelican
